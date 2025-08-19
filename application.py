b9db470767e5aa74f15735043f626b8a51e78077
Marcelo W Filho <marcelo.filho@volvo.com>
2025-08-19 20:08:47 -0300

    add-todo

--- application.py
+++ application.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, FastAPI
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 from core.logging import setup_logging
-from routers import routes_encode, routes_decode, routes_health
+from routers import routes_encode, routes_decode, routes_health, routes_screper
 from contextlib import asynccontextmanager
 from sentence_transformers import SentenceTransformer
 from core.config import settings
@@ -19,7 +19,7 @@ async def lifespan(app: FastAPI):
         EncoderService.set_model(model)
         logger.info("Modelo de embedding carregado.")
     except Exception as e:
-        logger.error(f"Falha ao carregar o modelo de embedding: {e}")
+        logger.error(f"Falha ao carregar modelo de embedding: {e}")
 
     yield
     logger.info("Desligando o servidor...")
@@ -37,3 +37,4 @@ app.include_router(router)
 app.include_router(routes_encode.router, prefix="/encode", tags=["Encode"])
 app.include_router(routes_decode.router, prefix="/decode", tags=["Decode"])
 app.include_router(routes_health.router, prefix="/health", tags=["Health"])
+app.include_router(routes_screper.router, prefix="/scraper", tags=["Scraper"])
---
+++ todo.md
@@ -0,0 +1,9 @@
+# TOTO
+
+- stemming
+- lematização
+- stopwords
+- espreções regulares (REGEX)
+- construção de um corpos
+- web scraping
+- recuperação de textos em redes sociais
